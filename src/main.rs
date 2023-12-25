52d6285418618ff1cb5913b67f01064a62dc2fae
Lucas Brendel <lucasbrendel@gmail.com>
2023-12-25 06:47:32 -0600

    fix: get a working setup

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@
 #![no_main]
 
 extern crate panic_semihosting;
-
+use hal::pac;
+use xmc4_hal as hal;
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
+
+    let _p = pac::Peripherals::take().unwrap();
     loop {}
 }
